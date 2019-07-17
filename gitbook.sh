e50e3cb12389b8fb032c5aedd898fa07a2afb0dd
coalchan <coalchan@gmail.com>
2019-07-17 16:33:49 +0800

    update: scripts

--- gitbook.sh
+++ gitbook.sh
@@ -9,6 +9,7 @@ if [[ -z "$msg" ]]; then
 fi
 
 # 首次需要运行 gitbook install 安装 book.json 中要到的插件
+# 还要安装 gh-pages 工具：npm install gh-pages -g
 # 如果部署站点为 https 的话，还需要修改 cnzz 的目标地址为 https，具体如下
 # 修改 node_modules\gitbook-plugin-cnzz\book\plugin.js，将 s95.cnzz.com 前的 http 改为 https
 
